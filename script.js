9b02967c7e0bc7227543c491ff94df7a0d462606
Rachel Sokol <50994670+rmsokol@users.noreply.github.com>
2023-04-18 01:29:49 -0400

    revert to orginal

--- script.js
+++ script.js
@@ -5,18 +5,21 @@ const slideshowContainer = document.querySelector("#slideshow-container")
 const present = document.querySelector("#present")
 const currentIndex = 0
 
-
 // event listener for clicking on present
 
-present.addEventListener("click", showRandomImage())
+present.addEventListener("click", showRandomImage)
 
 // function using random index and interating array to html container
 function showRandomImage() {
-    const currentIndex = Math.floor(Math.random()*images.length)
-    document.getElementById("present").style.display="none"
+    const currentIndex = Math.floor(Math.random()*     images.length)
+  document.getElementById("present").style.display="none"
     slideshowContainer.innerHTML = `<img class="imgs center" src="${images[currentIndex]}">`
-    
+  // timing method calling above function
+setInterval(showRandomImage, 2000)
 }
+
+
+
 
 
 
