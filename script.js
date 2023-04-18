380602c20d0d0d244de3fee63920b7541f429370
Rachel Sokol <50994670+rmsokol@users.noreply.github.com>
2023-04-18 01:22:34 -0400

    moved timing

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ images = ["images/imageA.jpg","images/imageB.jpg","images/imageC.jpg","images/im
 const slideshowContainer = document.querySelector("#slideshow-container")
 const present = document.querySelector("#present")
 const currentIndex = 0
-let slideshow = setInterval(showRandomImage, 2000) // timing method calling above function
+
 
 // event listener for clicking on present
 
@@ -12,6 +12,7 @@ present.addEventListener("click", showRandomImage())
 
 // function using random index and interating array to html container
 function showRandomImage() {
+    let slideshow = setInterval(showRandomImage, 2000) // timing method calling above function
     const currentIndex = Math.floor(Math.random()*images.length)
     document.getElementById("present").style.display="none"
     slideshowContainer.innerHTML = `<img class="imgs center" src="${images[currentIndex]}">`
